46a96cd9887b72fb0f150131a0634e2906e03481
Trumon A Latimer <144071590+Trumon@users.noreply.github.com>
2023-09-14 09:26:42 -0400

    Trumon's Josef Albers replicate

--- sketch.js
+++ sketch.js
@@ -3,5 +3,12 @@ function setup() {
 }
 
 function draw() {
-  background(125);
+  background(134, 187, 231);
+  rectMode(CENTER);
+  fill(47,102,65);
+  rect(200,220,330,330);
+  fill(47,125,65);
+  rect(200,250,250,250);
+  fill(5,12,100);
+  rect(200, 280, 160, 160);
 }
